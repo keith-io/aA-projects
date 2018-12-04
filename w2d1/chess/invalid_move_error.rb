b1c6d1cf7b8d72a74d0169eea4a43b734d163bb5
🦄 keith-io <34753004+keith-io@users.noreply.github.com>
2018-12-03 20:47:28 -0500

    Remove unnecessary error class files

--- w2d1/chess/invalid_move_error.rb
+++
@@ -1,5 +0,0 @@
-class InvalidMoveError < StandardError
-  def message
-    "That's not a valid move"
-  end
-end
--- w2d1/chess/no_piece_error.rb
+++
@@ -1,5 +0,0 @@
-class NoPieceError < StandardError
-  def message
-    puts "Ooops, there isn't a piece there"
-  end
-end
