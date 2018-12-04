8ab3037f19774881d7f6735fce305ea48921c9cf
keith-io <34753004+keith-io@users.noreply.github.com>
2018-12-04 17:31:50 -0500

    Refactor rendoring

--- w2d2/display.rb
+++ w2d2/display.rb
@@ -3,13 +3,14 @@ require_relative 'cursor'
 require 'colorize'
 
 class Display
-  attr_accessor :semaphore
+  attr_accessor :semaphore, :selected_pos
   attr_reader :board, :cursor
 
   def initialize(board)
     @cursor = Cursor.new([0,0], board)
     @board = board
     @semaphore = true
+    @selected_pos = nil
   end
 
   def test_loop
@@ -27,19 +28,37 @@ class Display
     board.grid.each_with_index do |row, idx1|
       toggle_semaphore
       row.each_with_index do |square, idx2|
-        if cursor.cursor_pos == [idx1, idx2]
-          print "  ".colorize( :background => :blue)
+
+        # if selected_pos == [idx1, idx2] && cursor.selected
+        #   print_pink_cursor
+        # #   toggle_semaphore
+        # end
+
+        # Fix this tomorrow
+        if cursor.selected && selected_pos == [idx1, idx2]
+          @selected_pos = [idx1, idx2]
+          print_pink_cursor
+          toggle_semaphore
+          next
+        else
+          @selected_pos = nil
+        end
+
+        if cursor.cursor_pos == [idx1, idx2] 
+          print_blue_cursor
           toggle_semaphore
           next
         end
 
         if semaphore 
-          print "  ".colorize( :background => :light_black)
+          render_grey_square
           toggle_semaphore
         else
-          print "  ".colorize( :background => :cyan)
+          render_cyan_square
           toggle_semaphore
         end
+
+
       end
       puts    
     end
@@ -47,5 +66,21 @@ class Display
 
   def toggle_semaphore
     self.semaphore = !semaphore
+  end
+
+  def print_blue_cursor
+    print "  ".colorize( :background => :blue)
+  end
+
+  def print_pink_cursor
+    print "  ".colorize( :background => :light_magenta)
+  end
+  
+  def render_grey_square
+    print "  ".colorize( :background => :light_black)
+  end
+
+  def render_cyan_square
+    print "  ".colorize( :background => :cyan)
   end
 end
