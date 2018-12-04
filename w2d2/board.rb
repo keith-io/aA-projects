bc6f3b9580547b60b19b2274d7ac8c58ebc2175c
U+1F984keith-io <34753004+keith-io@users.noreply.github.com>
2018-12-04 12:01:15 -0500

    Refactor initialize, create populate

--- w2d2/board.rb
+++ w2d2/board.rb
@@ -3,22 +3,25 @@ require_relative 'piece.rb'
 require_relative 'null_piece.rb'
 
 class Board
-  # attr_reader :grid
+  attr_reader :null
   def initialize
     @grid = Array.new(8) { Array.new(8) }
+    @null = NullPiece.instance
+    populate
+    p @board
+  end
+
+  def populate    
     @grid.each_with_index do |rank, idx|
       rank.each_with_index do |space, idx2|
 
         if idx < 2 || idx > 5
-          @grid[idx][idx2] = Piece.new(:generic)
+          self[[idx,idx2]] = Piece.new(:generic)
         else
-          # debugger
-          @grid[idx][idx2] = NullPiece.instance
-
+          self[[idx,idx2]] = @null
         end
       end
     end
-    p @board
   end
 
   def move_piece(start, finish)
@@ -28,7 +31,7 @@ class Board
       raise "That is not a valid move."
     else
       self[finish] = self[start]
-      self[start] = NullPiece.instance
+      self[start] = null
     end
   end
 
