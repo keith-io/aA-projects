5d7e25619c1d1c45ffa160c030eea84592b173ae
d^_^b <34753004+keith-io@users.noreply.github.com>
2018-12-07 15:11:21 -0500

    Finish phase III HashSet

--- w2d5/my-hash-map/lib/p03_hash_set.rb
+++ w2d5/my-hash-map/lib/p03_hash_set.rb
@@ -7,18 +7,34 @@ class HashSet
   end
 
   def insert(key)
+    unless self[key.hash].include?(key)
+      if @count == num_buckets
+        resize!
+        self[key.hash] << key
+        @count += 1
+      else
+        self[key.hash] << key
+        @count += 1
+      end
+    end
   end
-
+  
   def include?(key)
+    self[key.hash].include?(key)
   end
-
+  
   def remove(key)
+    if self[key.hash].include?(key)
+      self[key.hash].delete(key) 
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +42,13 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.each do |bucket|
+      until bucket.empty?
+        insert(bucket.pop)
+      end
+    end
   end
 end
