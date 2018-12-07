67f90e188c5160947b1362ea98404da19016fd73
d^_^b <34753004+keith-io@users.noreply.github.com>
2018-12-07 14:32:04 -0500

    Implement hashing functions

--- w2d5/my-hash-map/lib/p02_hashing.rb
+++ w2d5/my-hash-map/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    arr = self
+    id = 1
+    arr.each_with_index do |ele, i|
+      id = ((ele).hash * i) % 20
+    end
+    id
   end
 end
 
 class String
   def hash
+    # id = 0
+    # self.each_char.with_index do |char, i|
+    #   id += (char.ord).hash * i 
+    # end
+    # id
+    new_string = self.chars.map { |ele| ele = ele.ord }
+    new_string.hash
   end
 end
 
@@ -16,6 +29,15 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    id = 0
+    self.each do |key, value|
+      # p "this is the key #{key}"
+      # p "this is the value #{value}"
+      # p "hashing key: #{key.to_s.hash}"
+      # p "hashing value: #{value.to_s.hash}"
+      id += (key.hash + value.hash)
+      # p id
+    end
+    id
   end
 end
