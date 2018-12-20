7c7e87db53ab36c18da6e935b138123e95875bd6
🦄 keith-io <34753004+keith-io@users.noreply.github.com>
2018-12-20 11:20:47 -0500

    edit sessions

---
+++ test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE plays (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  year INTEGER NOT NULL,
+  playwright_id INTEGER NOT NULL
+
+  FOREIGN KEY (playwright
+);
--- w4d3/ninety-nine_cats_II_auth/app/controllers/sessions_controller.rb
+++ w4d3/ninety-nine_cats_II_auth/app/controllers/sessions_controller.rb
@@ -6,18 +6,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_credentials(
-      params[:user][:user_name],
-      params[:user][:password]
-    )
+    # wait this should be User.new, not user.find_by right?
+    # we wanna CREATE a user, then login. Not find a user... no?
 
-    if user.nil?
-      flash.now[:errors] = ['Incorrect username and/or password']
-      render :new
-    else
-      login!(user)
-      redirect_to cats_url
-    end
+  #   user = User.find_by_credentials(
+  #     params[:user][:user_name],
+  #     params[:user][:password]
+  #   )
+
+  #   if user.nil?
+  #     flash.now[:errors] = ['Incorrect username and/or password']
+  #     render :new
+  #   else
+  #     login!(user)
+  #     redirect_to cats_url
+  #   end
   end
 
   def destroy
