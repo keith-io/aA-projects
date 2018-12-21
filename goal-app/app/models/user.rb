0d64b01e255af03246444a7cba5acb90db1de7fc
🦄 keith-io <34753004+keith-io@users.noreply.github.com>
2018-12-21 18:06:57 -0500

    Add user associations and method tests

--- goal-app/app/models/user.rb
+++ goal-app/app/models/user.rb
@@ -5,12 +5,25 @@ class User < ApplicationRecord
 
     attr_reader :password
 
+    has_many :goals
+
     def password=(password)
         @password = password
         self.password_digest = BCrypt::Password.create(password)
     end
 
     def ensure_session_token
-        self.session_token = SecureRandom.urlsafe_base64
+        self.session_token ||= SecureRandom.urlsafe_base64
     end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        user && user.is_password?(password) ? user : nil        
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+
 end
--- goal-app/db/schema.rb
+++ goal-app/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_191426) do
+ActiveRecord::Schema.define(version: 2018_12_21_215957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
