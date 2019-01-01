ad3caab62eb8c40e4dc182aef812bf34e8fabe27
d^_^b <34753004+keith-io@users.noreply.github.com>
2019-01-01 16:40:49 -0500

    Add comment associations

--- w5d2/reddit_clone/app/models/comment.rb
+++ w5d2/reddit_clone/app/models/comment.rb
@@ -2,7 +2,11 @@ class Comment < ApplicationRecord
   validates :body, presence: true
 
   belongs_to :user
-  belongs_to :sub
+  belongs_to :post
+  belongs_to :parent_comment,
+    foreign_key: :parent_comment_id,
+    class_name: :comment,
+    optional: :true
 
-  
+
 end
