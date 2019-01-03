15a63aba3d76093e54cce9a7f86fea74ee79cd29
d-_^b <34753004+keith-io@users.noreply.github.com>
2019-01-03 15:17:25 -0500

    implement phase 2

--- w5d4/introjs/phase_1_arrays.js
+++ w5d4/introjs/phase_1_arrays.js
@@ -6,3 +6,33 @@ Array.prototype.uniq = function () {
   }
   return Object.keys(hash);
 };
+
+Array.prototype.twoSum = function () {
+  const hash = {};
+  const pairs = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if ( hash[ 0 - this[i]] ) {
+      pairs.push([this.indexOf(0 - this[i]), i]);
+    } else {
+      hash[this[i]] = true;
+    }
+  }
+
+  return pairs;
+};
+
+Array.prototype.transpose = function () {
+
+  const arr = Array((this[0].length)).fill().map(el => []);
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = 0; j < this[i].length; j++) {
+      debugger
+      arr[j].push(this[i][j]);
+    }
+  }
+  return arr;
+};
+
+//array[0].map((col, i) => array.map(row => row[i]));
