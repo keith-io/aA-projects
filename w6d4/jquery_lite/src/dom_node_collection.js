81d2fe73f70095a2a14a3ce752ad1938063f4c48
d^_-b <34753004+keith-io@users.noreply.github.com>
2019-01-10 15:36:06 -0500

    Implement DOMNodeCollection class

---
+++ w6d4/jquery_lite/src/dom_node_collection.js
@@ -0,0 +1,84 @@
+class DOMNodeCollection {
+  constructor(elements) {
+    this.elements = elements;
+    //The core function should return an instance of DOMNodeCollection.??
+  }
+  html(str) {
+    if (str) {
+      this.elements.forEach(element => {
+        element.innerHTML = str;
+      });
+    } else {
+      return this.elements[0].innerHTML;
+    }
+  }
+
+  empty() {
+    this.elements.forEach(el => {
+      el.innerHTML = '';
+    });
+  }
+
+  append(collection) {
+    collection.forEach(tag => {
+      this.elements.forEach(el => {
+        el.innerHTML += tag.outerHTML;
+      });
+    });
+  }
+
+  // second time around like a boss
+  attr(attribute){
+    for(let i = 0; i < this.elements.length; i++) {
+      const el = this.elements[i];
+      if (el.outerHTML.includes(attribute)) {
+        let atts = el.attributes;
+        return atts[attribute].value;
+      }
+    }
+  }
+
+  // first attempt
+  // attr(attribute) {
+  //   let result;
+  //   this.elements.forEach((el) => {
+  //     if (el.outerHTML.includes(attribute)) {
+  //       let arr = el.outerHTML.split("\"");
+  //       for (let i = 0; i < arr.length; i++) {
+  //         if (arr[i].includes(attribute)) {
+  //           result = arr[i + 1];
+  //         }
+  //       }
+  //     }
+  //   });
+  //   return result;
+  // }
+
+  // addClass(klass) {
+  //   for(let i = 0; i < this.elements.length; i++) {
+  //     let el = this.elements[i];
+  //     debugger
+  //     if(el.outerHTML.includes('class')) {
+  //       debugger
+  //       el.attributes['class'].value += ` ${klass}`;
+  //     } else {
+  //       el.attributes['class'] = ` ${klass}`;
+  //     }
+  //   }
+  //   return this;
+  // }
+  addClass(klass) {
+    this.elements.forEach(el => {
+      el.classList.add(klass);
+    });
+  }
+
+  removeClass(klass) {
+    this.elements.forEach(el => {
+      el.classList.remove(klass);
+    });
+  }
+}
+
+module.exports = DOMNodeCollection;
+
