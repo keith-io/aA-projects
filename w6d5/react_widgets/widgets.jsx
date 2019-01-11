a9b4eb64fb5e3ea6503b3fc8fe8211f5a2e4a831
d^_-b <34753004+keith-io@users.noreply.github.com>
2019-01-11 18:36:33 -0500

    Correct widgets rendering to root

--- w6d5/react_widgets/widgets.jsx
+++ w6d5/react_widgets/widgets.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './frontend/clock';
+import Tabs from './frontend/tabs'
 
+const panes = [
+  { title: "first", content: "i am a pane" }, 
+  { title: "second", content: "woop woop woop" }, 
+  { title: "3", content: "something" }
+];
 
-// / function Root() {
-//   return (
-//     <div>
-//       <Clock />
-//       {/* <Weather />
-//       <div className='interactive'>
-//         <Tabs panes={panes} />
-//         <AutoComplete names={names} />
-//       </div> */}
-//     </div>
-//   );
-// }/
+function Root() {
+  return (
+    <div>
+      <Clock />
+      {/* <Weather /> */}
+      <div className='interactive'>
+        <Tabs panes={panes} />
+        {/* <AutoComplete names={names} /> */}
+      </div>
+    </div>
+  );
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   // ReactDOM.render(<component_name>, root);
-  // ReactDOM.render(<component_name>, root);
-  ReactDOM.render(<Clock />, root);
+  ReactDOM.render(<Root />, root);
+  // only root
+  // ReactDOM.render(<Tabs panes={panes} />, root);
 });
-
