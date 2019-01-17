0f808b61e626876ce73df2145b68512bf6383242
d^_^b <34753004+keith-io@users.noreply.github.com>
2019-01-17 18:00:19 -0500

    Implement ajax fetchers in api_util

--- w7d4/pokedex_2/frontend/util/api_util.js
+++ w7d4/pokedex_2/frontend/util/api_util.js
@@ -4,4 +4,11 @@ export const fetchAllPokemon = () => {
     url: "/api/pokemon"
     // url: "../../app/assets/views/api/pokemon"
   });
-}
+};
+
+export const fetchSinglePokemon = (id) => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/pokemon/${id}`
+  });
+};
