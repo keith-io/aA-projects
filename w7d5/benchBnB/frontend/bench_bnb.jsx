686f8e9d6f70e21a362cb30a0e5f862e23a2c96b
d^_-b <34753004+keith-io@users.noreply.github.com>
2019-01-18 15:24:41 -0500

    Setup StaticPagesController, backend user auth

---
+++ w7d5/benchBnB/frontend/bench_bnb.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Bench-B-no-Breakfast</h1>, root);
+});
