03f19a437e8bd171508a1bacb1ab2005bf6f568a
d^_-b <34753004+keith-io@users.noreply.github.com>
2019-01-18 17:28:06 -0500

    Implement session ApiUtil

--- w7d5/benchBnB/frontend/bench_bnb.jsx
+++ w7d5/benchBnB/frontend/bench_bnb.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    window.login = login;
     ReactDOM.render(<h1>Bench-B-no-Breakfast</h1>, root);
 });
